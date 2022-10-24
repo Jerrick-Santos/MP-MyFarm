649fb651e40298adc86704aa2afd80b430120a1e
carlo-reyes <carlo_jose_reyes@dlsu.edu.ph>
2022-10-24 16:20:11 +0800

    Added 'extends' throughout all seeds -- Apple, Carrot, Mango, Potato, Rose, Sunflower, Turnip, Turnips.

--- Game/Rose.java
+++ Game/Rose.java
@@ -1,4 +1,7 @@
 package Game;
 
-public class Rose {
+public class Rose extends Seed{
+    public Rose(String name, String cropType, int harvestDayRequired, int daysPassed, int waterMin, int waterMax, int fertilizerMin, int fertilizerMax, int seedCost, int baseSellingPrice, double expYield) {
+        super(name, cropType, harvestDayRequired, daysPassed, waterMin, waterMax, fertilizerMin, fertilizerMax, seedCost, baseSellingPrice, expYield);
+    }
 }
--- Game/Turnips.java
+++ Game/Turnips.java
@@ -1,4 +1,7 @@
 package Game;
 
-public class Turnips {
+public class Turnips extends Seed{
+    public Turnips(String name, String cropType, int harvestDayRequired, int daysPassed, int waterMin, int waterMax, int fertilizerMin, int fertilizerMax, int seedCost, int baseSellingPrice, double expYield) {
+        super(name, cropType, harvestDayRequired, daysPassed, waterMin, waterMax, fertilizerMin, fertilizerMax, seedCost, baseSellingPrice, expYield);
+    }
 }
