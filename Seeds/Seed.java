949ef269cc3030b2bd9da7b4d5b574a726cf2deb
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2022-10-24 12:35:28 +0800

    Edited Seed.java - added attributes and methods

--- Seeds/Seed.java
+++ Seeds/Seed.java
@@ -1,4 +1,63 @@
 package Seeds;
 
 public class Seed {
+    private String name;
+    private String cropType;
+    private String harvestDayRequired;
+    private int daysPassed;
+    private Water water;
+    private Fertilizer fertilizer;
+    private int seedCost;
+    private int baseSellingPrice;
+    private double expYield;
+    private boolean withered;
+
+    public void addDaysPassed(){
+        this.daysPassed++;
+    }
+    
+
+    public void setWithered(boolean withered) {
+        this.withered = withered;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCropType() {
+        return cropType;
+    }
+
+    public String getHarvestDayRequired() {
+        return harvestDayRequired;
+    }
+
+    public int getDaysPassed() {
+        return daysPassed;
+    }
+
+    public Water getWater() {
+        return water;
+    }
+
+    public Fertilizer getFertilizer() {
+        return fertilizer;
+    }
+
+    public int getSeedCost() {
+        return seedCost;
+    }
+
+    public int getBaseSellingPrice() {
+        return baseSellingPrice;
+    }
+
+    public double getExpYield() {
+        return expYield;
+    }
+
+    public boolean isWithered() {
+        return withered;
+    }
 }
