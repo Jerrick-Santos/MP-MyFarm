6965bdd3068853bb41cb9e46bb4f58da5fc0a2cd
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2022-10-24 13:29:25 +0800

    Created content for Water.java and Fertilizer.java. Added consturctor in Seed.java

--- Game/Seed.java
+++ Game/Seed.java
@@ -12,6 +12,20 @@ public class Seed {
     private double expYield;
     private boolean withered;
 
+    public Seed(String name, String cropType, String harvestDayRequired, int daysPassed, int waterMin, int waterMax, int fertilizerMin, int fertilizerMax,
+                int seedCost, int baseSellingPrice, double expYield) {
+        this.name = name;
+        this.cropType = cropType;
+        this.harvestDayRequired = harvestDayRequired;
+        this.daysPassed = 0;
+        this.water = new Water(waterMin, waterMax);
+        this.fertilizer = new Fertilizer(fertilizerMin, fertilizerMax);
+        this.seedCost = seedCost;
+        this.baseSellingPrice = baseSellingPrice;
+        this.expYield = expYield;
+        this.withered = false;
+    }
+
     public void addDaysPassed(){
         this.daysPassed++;
     }
