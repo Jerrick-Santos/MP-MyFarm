445b26a17cbd1bbf943c3ec36967d42db277d55c
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2022-10-26 12:48:02 +0800

    Added notification of add/deduct on exp and balance on GameStats.java

--- Game/GameStats.java
+++ Game/GameStats.java
@@ -10,14 +10,20 @@ public class GameStats {
     }
     public void addWallet(double amt){
         this.balance += amt;
+        System.out.println("Balance added : +" + amt);
+        System.out.println("Your wallet has: " + this.balance);
     }
 
     public void deductWallet(double amt){
         this.balance -= amt;
+        System.out.println("Balance deducted : -" + amt);
+        System.out.println("Your wallet has: " + this.balance);
     }
 
     public void gainExp(double amt){
         this.exp += amt;
+        System.out.println("Exp added : +" + amt);
+        System.out.println("Exp: " + this.exp);
     }
 
     public double getBalance() {
