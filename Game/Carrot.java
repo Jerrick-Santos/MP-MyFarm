37c40e4ff4cfe9b3d4fca446e6451476c2de332c
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2022-10-30 23:51:10 +0800

    Edited constructor for child classes from Seed.java

--- Game/Carrot.java
+++ Game/Carrot.java
@@ -1,7 +1,7 @@
 package Game;
 
 public class Carrot extends Seed{
-    public Carrot(String name, String cropType, int harvestDayRequired, int daysPassed, int waterMin, int waterMax, int fertilizerMin, int fertilizerMax, int seedCost, int baseSellingPrice, double expYield) {
-        super(name, cropType, harvestDayRequired, daysPassed, waterMin, waterMax, fertilizerMin, fertilizerMax, seedCost, baseSellingPrice, expYield);
+    public Carrot(String name, String cropType, int harvestDayRequired, int waterMin, int waterMax, int fertilizerMin, int fertilizerMax, int seedCost, int productsProducedMin, int productsProducedMax, int baseSellingPrice, double expYield) {
+        super(name, cropType, harvestDayRequired, waterMin, waterMax, fertilizerMin, fertilizerMax, seedCost, productsProducedMin, productsProducedMax, baseSellingPrice, expYield);
     }
 }
--- Game/Rose.java
+++ Game/Rose.java
@@ -1,7 +1,7 @@
 package Game;
 
 public class Rose extends Seed{
-    public Rose(String name, String cropType, int harvestDayRequired, int daysPassed, int waterMin, int waterMax, int fertilizerMin, int fertilizerMax, int seedCost, int baseSellingPrice, double expYield) {
-        super(name, cropType, harvestDayRequired, daysPassed, waterMin, waterMax, fertilizerMin, fertilizerMax, seedCost, baseSellingPrice, expYield);
+    public Rose(String name, String cropType, int harvestDayRequired, int waterMin, int waterMax, int fertilizerMin, int fertilizerMax, int seedCost, int productsProducedMin, int productsProducedMax, int baseSellingPrice, double expYield) {
+        super(name, cropType, harvestDayRequired, waterMin, waterMax, fertilizerMin, fertilizerMax, seedCost, productsProducedMin, productsProducedMax, baseSellingPrice, expYield);
     }
 }
