ec791f2a5cf9bb1c53ce57b34ad27a968f3fa7ab
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2022-11-24 09:15:04 +0800

    GUI Branch Initial Commit

--- Game/Model/Seeds/Tulips.java
+++
@@ -1,9 +0,0 @@
-package Game.Model.Seeds;
-
-import Game.Model.Controller.FarmerType;
-
-public class Tulips extends Seed{
-    public Tulips(String name, String cropType, int harvestDayRequired, int waterMin, int waterMax, int fertilizerMin, int fertilizerMax, int seedCost, int productsProducedMin, int productsProducedMax, int baseSellingPrice, double expYield, FarmerType farmerType) {
-        super(name, cropType, harvestDayRequired, waterMin, waterMax, fertilizerMin, fertilizerMax, seedCost, productsProducedMin, productsProducedMax, baseSellingPrice, expYield, farmerType);
-    }
-}
